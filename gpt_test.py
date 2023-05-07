52d03b910d5e8d12a92eabfeb03528bf3d058c8d
Joon <wnstjq0915@gmail.com>
2023-05-08 01:04:05 +0900

    넘피 정리

--- gpt_test.py
+++ gpt_test.py
@@ -16,10 +16,10 @@ import numpy as np
 # axis=1은 행끼리(가로로만) 진행
 
 
-c = np.array([[0,1,2],[3,4,5]])
-d = np.array([[6,7,8],[9,10,11]])
+# c = np.array([[0,1,2],[3,4,5]])
+# d = np.array([[6,7,8],[9,10,11]])
 
-print(np.concatenate((c,d),axis=0))
+# print(np.concatenate((c,d),axis=0))
 
 # axis0
 # [[ 0  1  2]
@@ -30,3 +30,30 @@ print(np.concatenate((c,d),axis=0))
 # axis1
 # [[ 0  1  2  6  7  8]
 # [ 3  4  5  9 10 11]]
+
+
+# ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
+# import numpy as np
+# # a = np.array([1, 2, 3])
+# # b = np.array([4, 5, 6])
+# # c = np.array([7, 8, 9])
+# # d = np.column_stack((a, b, c))
+# # print(d)
+# a = np.array([1, 2, 3, 4])
+
+import numpy as np
+
+# arr = np.array([1, 2, 3, 4, 5])
+# print(arr.mean())  # Output: 3.0
+
+# arr = np.array([[1, 2], [3, 4]])
+# print(arr.mean(axis=0))  # Output: [2. 3.]
+
+# arr = np.array([[1, 2], [3, 4]])
+# print(arr.mean(axis=1))  # Output: [1.5 3.5]
+
+xarr = np.array([100,200,300,400])
+yarr = np.array([1,2,3,4])
+cond = np.array([True,False,True,False])
+print(np.where(xarr>200,max(xarr),0))
