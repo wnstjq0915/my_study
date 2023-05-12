a5efccbc808fbdfb20f6ecc2b4a5d29f41de408f
wnstjq0915 <wnstjq0915@gmail.com>
2023-05-12 18:11:24 +0900

    문자열 startswith 추가

--- python_study/test4.py
+++ python_study/test4.py
@@ -1,3 +1,14 @@
-a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-for i, j, k in a:
-    print(i, j, k)
+# a = '가나다'
+# print(a.startswith('가')) # True
+
+# a = '가나다'
+# print(a.contains('나'))
+
+# a = ['가나다', '가다라', '라마바']
+# print(a.startswith('가')) # 에러
+
+a = [1, 2, 3, 4, 5]
+print(5 in a) # True
+
+a = 'Hello'
+print('e' in a) # True
