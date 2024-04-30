5417ff16fea3010671234d899501904f92a9f973
wnstjq <wnstjq0915@gmail.com>
2024-04-30 18:36:02 +0900

    문제 마저 풀기

--- 백준문제/실버/숫자카드_10815 copy.py
+++ 백준문제/실버/숫자카드_10815 copy.py
@@ -1,16 +1,28 @@
-def dichotomy(li, num):
-    i = len(li) - 1
+import math
 
-    while i != 0:
+def dichotomy(li, num):
+    len_li = len(li) - 1
+    add_i = len_li
+    i = add_i
+
+    if num < li[0] or num > li[add_i]:
+        return "0"
+
+    print(li)
+    print(num)
+    while add_i != 0:
+        if i < 0 or i > len_li:
+            return "0"
+        # add_i //= 2 # 올림으로 바꾸기
+        add_i = math.ceil(add_i/2)
+        print(i, add_i)
         if num > li[i]:
-            li = li[i:]
             # 슬라이싱 할 때마다 슬라이싱된 리스트가 생성되며
             # O(k)의 시간이 소요되기 때문에(k는 슬라이스 크기)
             # 비효율적
-            i = len(li) // 2
+            i -= add_i
         elif num < li[i]:
-            li = li[:i+1]
-            i = (len(li) - 1) // 2
+            i += add_i
         else:
             return "1"
     return "1" if li[i] == num else "0"
