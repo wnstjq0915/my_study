71f5012fa0991dd055ced29856200967ce7e901f
wnstjq <wnstjq0915@gmail.com>
2024-05-01 18:11:44 +0900

    문제1 완료

--- 백준문제/실버/숫자카드_10815.py
+++ 백준문제/실버/숫자카드_10815.py
@@ -1,19 +1,28 @@
 def dichotomy(li, num):
     i = len(li) - 1
 
-    while i != 0:
+    if num < li[0] or num > li[i]:
+        return "0"
+    elif num == li[0] or num == li[i]:
+        return "1"
+
+    min_i, max_i = 0, i
+    add_i = 1
+
+    # 기존코드는 슬라이싱을 활용하여 리스트 자체를 반씩 자름
+    # 슬라이싱 할 때마다 슬라이싱된 리스트가 생성되며
+    # O(k)의 시간이 소요되기 때문에(k는 슬라이스 크기)
+    # 비효율적
+    while add_i != 0:
         if num > li[i]:
-            li = li[i:]
-            # 슬라이싱 할 때마다 슬라이싱된 리스트가 생성되며
-            # O(k)의 시간이 소요되기 때문에(k는 슬라이스 크기)
-            # 비효율적
-            i = len(li) // 2
+            min_i = i
         elif num < li[i]:
-            li = li[:i+1]
-            i = (len(li) - 1) // 2
+            max_i = i
         else:
             return "1"
-    return "1" if li[i] == num else "0"
+        add_i = (max_i-min_i) // 2
+        i = min_i+add_i
+    return "0"
 
 input()
 a = list(set(map(int, input().split())))
@@ -22,7 +31,5 @@ a.sort() # O(n log n)의 시간 소요
 input()
 b = list(map(int, input().split()))
 
-ans = ""
 for num in b:
-    ans += dichotomy(a, num) + " "
-print(ans[:-1])
+    print(dichotomy(a, num), end=" ")
